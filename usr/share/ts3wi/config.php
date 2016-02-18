3d6b47938c312778686a6d83aaabe2ceb83177a6
tekkbebe <wehrmacht1986@yahoo.com>
2016-02-18 16:04:46 -0500

    Add ts3wi

--- usr/share/ts3wi/config.php
+++ usr/share/ts3wi/config.php
@@ -2,17 +2,17 @@
 /*
 *Copyright (C) 2012-2013  Psychokiller
 *
-*This program is free software; you can redistribute it and/or modify it under the terms of
-*the GNU General Public License as published by the Free Software Foundation; either
+*This program is free software; you can redistribute it and/or modify it under the terms of 
+*the GNU General Public License as published by the Free Software Foundation; either 
 *version 3 of the License, or any later version.
 *
-*This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-*without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+*without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
 *See the GNU General Public License for more details.
 *
-*You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>.
+*You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>. 
 */
-if(!defined("SECURECHECK")) {die($lang['error_file_alone']);}
+if(!defined("SECURECHECK")) {die($lang['error_file_alone']);} 
 /*
 REGARD!!
 If you use the web interface, they must write the webserver ip in the query_ip_whitelist.txt.
@@ -33,31 +33,23 @@ $server[2]['ip']= "127.0.0.3";
 $server[2]['tport']= "30033";
 */
 
-$server[0]['alias']= "TS3 Server";
+$server[0]['alias']= "Lokaler Server";
 $server[0]['ip']= "127.0.0.1";
 $server[0]['tport']= 10011;
 
-//Language German = de, English = en, Netherlandish=nl (by pd1evl), French = fr (by supra63200)
-$cfglang = "en";
+$cfglang	=	"de";			//Language German = de, English = en, Netherlandish=nl (by pd1evl), French = fr (by supra63200)
 
-//Set the Limit for Clients show per Page on Client List
-$duration = "100";
+$duration = "100";				//Set the Limit for Clients show per Page on Client List
 
-//If true you can switch the Server on the header
-$fastswitch = true;
+$fastswitch=true;				//If true you can switch the Server on the header
 
-//Define the position where the icons on the Viewer will show left or right
-$showicons = "left";
+$showicons="left";				//Define the position where the icons on the Viewer will show left or right
 
-//Chose your design  set 'new' for the default design or the name of your own create design
-$style = "new";
+$style="new";					//Chose your design  set 'new' for the default design or the name of your own create design
 
-//This Name will be show if you send a message to a Server
-$msgsend_name = "Webinterface";
+$msgsend_name="Webinterface";	//This Name will be show if you send a message to a Server
 
-// Set it to false to not show the message of the day window
-$show_motd = true;
+$show_motd=true;				// Set it to false to not show the message of the day window
 
-// Set it to false to not show the Webinterface Version on the footer
-$show_version = true;
+$show_version=true;				// Set it to false to not show the Webinterface Version on the footer
 ?>
